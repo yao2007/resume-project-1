fcd125aecc5ec633a6ef92b905b96e297c78dcaf
YueYao <yueyao@ucdavis.edu>
2018-11-29 01:52:15 -0800

    clear

--- flightreservation/src/main/java/com/bharath/flightreservation/repos/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bharath.flightreservation.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bharath.flightreservation.entities.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
--- flightreservation/src/main/java/com/bharath/flightreservation/services/SecurityService.java
+++
@@ -1,7 +0,0 @@
-package com.bharath.flightreservation.services;
-
-public interface SecurityService {
-	
-	boolean login(String username,String password);
-
-}
